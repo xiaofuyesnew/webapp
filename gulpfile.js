591f6dac204ecfdcb3bc2bf0d7f5f03e650722c4
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-05-17 12:24:55 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 /**
+ * automatic working flow for fuqin project
  * 
+ * @function 
+ *      automatic compile sourse files to developing folder
+ *      or building release files to www folder
+ * 
+ * @author allen wong
+ * 
+ * @date   2017-05-15
+ * 
+ * @last-monify  2017-05-17
+ * 
+ * @logs
  * 
  */
 
@@ -19,6 +31,9 @@ const uglifyjs = require('gulp-uglifyjs')
 const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
 
+/**
+ * development part
+ */
 gulp.task('del:dev', () => {
     return del([
         './dev/**/*',
@@ -26,22 +41,10 @@ gulp.task('del:dev', () => {
     ])
 })
 
-gulp.task('del', () => {
-    return del([
-        './www/**/*',
-        '!./www/README.md'
-    ])
-})
-
 gulp.task('img:dev', () => {
-    return gulp.src('.src/img/**/*.*')
+    return gulp.src('.src/img/**/*')
         .pipe(gulp.dest('./dev/img'))
         .pipe(reload({ stream: true }))
-})
-
-gulp.task('img', () => {
-    return gulp.src('./src/img/**/*.*')
-        .pipe(gulp.dest('./www/img'))
 })
 
 gulp.task('sass:dev', () => {
@@ -51,27 +54,94 @@ gulp.task('sass:dev', () => {
         .pipe(reload({ stream: true }))
 })
 
-gulp.task('sass', () => {
-    return gulp.src('./src/style/sass/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' })
-            .on('error', sass.logError))
-        .pipe(gulp.dest('./www/css'))
+gulp.task('clean:dev', ['sass:dev'], () => {
+    return del([
+        './dev/css/mixin.css'
+    ])
 })
 
 gulp.task('lib:dev', () => {
-    return gulp.src('./src/lib/dev/**/*')
-        .pipe('./dev/lib/')
+    return gulp.src('./src/lib-dev/**/*')
+        .pipe(gulp.dest('./dev/lib/'))
+        .pipe(reload({ stream: true }))
 })
 
-gulp.task('lib:dev', () => {
-    return gulp.src('./src/lib/dev/**/*')
-        .pipe('./dev/lib/')
+gulp.task('js:dev', () => {
+    return gulp.src('./src/script/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./dev/js'))
+        .pipe(reload({ stream: true }))
 })
 
 gulp.task('index:dev', () => {
     return gulp.src('./src/html/index.html')
         .pipe(gulp.dest('./dev'))
         .pipe(reload({ stream: true }))
+})
+
+gulp.task('html:dev', () => {
+    return gulp.src(['./src/html/*.html', '!./src/html/index.html'])
+        .pipe(gulp.dest('./dev/html'))
+        .pipe(reload({ stream: true }))
+})
+
+gulp.task('dev', ['img:dev',
+        'lib:dev',
+        'clean:dev',
+        'js:dev',
+        'index:dev',
+        'html:dev'], () => {
+    browserSync.init({
+        server: {
+            baseDir: './dev'
+        }
+    })
+    gulp.watch('.src/img/**/*.*', ['img:dev'])
+    gulp.watch('./src/lib-dev/**/*.*', ['lib:dev'])
+    gulp.watch('./src/style/sass/*.scss', ['clean:dev'])
+    gulp.watch('./src/script/*.js', ['js:dev'])
+    gulp.watch('./src/html/*.html', ['index:dev', 'html:dev'])
+})
+/** --- development end --- */
+
+/**
+ * build release files
+ */
+gulp.task('del', () => {
+    return del([
+        './www/**/*',
+        '!./www/README.md'
+    ])
+})
+
+gulp.task('img', () => {
+    return gulp.src('./src/img/**/*')
+        .pipe(gulp.dest('./www/img'))
+})
+
+gulp.task('bundle', () => {
+    return gulp.src('./src/lib/**/*.js')
+        .pipe(concat('lib.min.js'))
+        .pipe(uglifyjs())
+        .pipe(gulp.dest('./www/js'))
+})
+
+gulp.task('libcss', () => {
+    return gulp.src('./src/lib/**/*.css')
+        .pipe(cleanCss({
+            advanced: false,
+            keepSpecialComments: '*'
+        }))
+        .pipe(gulp.dest('./www/css'))
+})
+
+gulp.task('sass', () => {
+    return gulp.src('./src/style/sass/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' })
+            .on('error', sass.logError))
+        .pipe(gulp.dest('./www/css'))
 })
 
 gulp.task('index', () => {
@@ -83,12 +153,6 @@ gulp.task('index', () => {
         .pipe(gulp.dest('./www'))
 })
 
-gulp.task('html:dev', () => {
-    return gulp.src(['./src/html/*.html', '!./src/html/index.html'])
-        .pipe(gulp.dest('./dev/html'))
-        .pipe(reload({ stream: true }))
-})
-
 gulp.task('html', () => {
     return gulp.src(['./src/html/*.html', '!./src/html/index.html'])
         .pipe(htmlReplace({
@@ -98,22 +162,15 @@ gulp.task('html', () => {
         .pipe(gulp.dest('./www/html'))
 })
 
-
-gulp.task('clean:dev', () => {
-    return del([
-        './dev/css/mixin.css'
-    ])
-})
-
 gulp.task('clean', () => {
     return del([
         './www/css/mixin.css'
     ])
 })
 
-gulp.task('dev', [], () => {
 
-})
+/** --- release end --- */
+
 
 gulp.task('build', [], () => {
 
--- src/script/index.js
+++ src/script/index.js
@@ -6,6 +6,6 @@
 var app = new Vue({
     el: '.app',
     data: {
-        message: 'Hello'
+        message: 'asdasdasdaaaa'
     }
 })
